19797bf3d302e962d2978319fd9be4953ed4e143
WCS19 <wcs@bu.edu>
2024-02-16 03:29:44 -0500

    updating event vs command logic

--- test-bot.py
+++ test-bot.py
@@ -1,31 +1,34 @@
 
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') 
 
+#Initialize intents
 intents = discord.Intents.default()
 intents.messages = True 
 intents.message_content = True  
 intents.reactions = True 
 
-client = discord.Client(intents=intents)
+#Initialize client and command prefix
+client = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} is now online.')
 
-@client.event
-async def on_message(message):
-    print(f"Received message from {message.author}: {message.content}")  # Debug print
+@client.command()
+async def hello(message):
     if message.author == client.user:
-        return
+        return   
+    await message.send('Hello, I am a test bot.')
 
-    if message.content == '!hello' or message.content =='hello':
-        print("Match found for '!hello/hello', sending response...")  # Debug print
-        await message.channel.send('Hello! I am a test bot.')
+def run_bot():
+    client.run(TOKEN)
 
-client.run(TOKEN)
+if __name__ == '__main__':
+    run_bot()
 
